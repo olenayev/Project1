42df633f0f3946ddffbe49d33da8cd80c5717164
olenayev <116585193+olenayev@users.noreply.github.com>
2022-11-02 15:58:02 +0100

    added new code

--- WorkWithFiles.py
+++ WorkWithFiles.py
@@ -16,3 +16,62 @@ print(df.loc[[0,1,2], "First Name" ])
 
 # To select the 0th,1st and 2nd row of "First Name" column only
 print(df.iloc[[0,1,2], 0])
+
+
+#############json
+
+import json
+person = {
+    'first_name' : 'Mark',
+    'last_name' : 'abc',
+    'age' : 27,
+    'address': {
+        "streetAddress": "21 2nd Street",
+        "city": "New York",
+        "state": "NY",
+        "postalCode": "10021-3100"
+    }
+}
+
+ # writing JSON object
+with open('person.json', 'w') as f: 
+    json.dump(person, f)
+
+# Serializing json  
+json_object = json.dumps(person, indent = 4) 
+  
+# Writing to sample.json 
+with open("sample.json", "w") as outfile: 
+    outfile.write(json_object) 
+
+  ######  XML file format
+
+import xml.etree.ElementTree as ET
+
+# create the file structure
+employee = ET.Element('employee')
+details = ET.SubElement(employee, 'details')
+first = ET.SubElement(details, 'firstname')
+second = ET.SubElement(details, 'lastname')
+third = ET.SubElement(details, 'age')
+first.text = 'Shiv'
+second.text = 'Mishra'
+third.text = '23'
+
+# create a new XML file with the results
+mydata1 = ET.ElementTree(employee)
+# myfile = open("items2.xml", "wb")
+# myfile.write(mydata)
+with open("new_sample.xml", "wb") as files:
+    mydata1.write(files)
+
+# Image files
+from PIL import Image 
+import urllib.request
+urllib.request.urlretrieve("https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg", "dog.jpg")
+
+# Read image 
+img = Image.open('dog.jpg') 
+  
+# Output Images 
+display(img)
