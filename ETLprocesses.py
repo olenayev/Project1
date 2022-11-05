2a01a7526f7cd60a2f0985704a27bd84c3565b3d
olenayev <116585193+olenayev@users.noreply.github.com>
2022-11-05 19:36:03 +0100

    commented

--- ETLprocesses.py
+++ ETLprocesses.py
@@ -1,4 +1,4 @@
 import glob                         # this module helps in selecting files 
 import pandas as pd                 # this module helps in processing CSV files
 import xml.etree.ElementTree as ET  # this module helps in processing XML files.
-from datetime import datetime
+###from datetime import datetime
